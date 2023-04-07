3b90853da2eb660ea855e0ab5df8b20149981001
Pete Kemp <pkemp1992@live.co.uk>
2023-04-07 21:20:01 +0100

    Test account with mock db

--- api/account.go
+++ api/account.go
@@ -52,9 +52,10 @@ func (server *Server) getAccount(ctx *gin.Context) {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
+
 	ctx.JSON(http.StatusOK, account)
 }
 
